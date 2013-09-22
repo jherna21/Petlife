e41e7c7a214d1be7eb9e156ce5beef37a8c03311
gescoba5 <gescobaruribe@gmail.com>
2013-09-22 18:07:26 -0500

    Navegación entre las diferentes vistas
    Todas las vistas con estilos

--- Back-End/logout.php
+++ Back-End/logout.php
@@ -2,8 +2,7 @@
 
     session_start();
     session_destroy();
-	echo "<script>alert('Est\u00e1 saliendo de PetLife');
-		window.location.href=\"../index.html\"</script>";
-    //header("Location: ../index.html");
+    echo "<script>alert('Est\u00e1 saliendo de PetLife');
+        window.location.href=\"../index.html\"</script>";
 
 ?>
